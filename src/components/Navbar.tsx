8463ffbcf6e87f61ac3e9a146a50f025f49297b4
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-10-03 23:23:29 +0600

    added the navbar responsive border for shape

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,16 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="trapezoid sticky top-0 z-50 mx-auto flex w-[70rem]
-     items-center justify-center gap-8 bg-[#F9F6EE] uppercase shadow-2xl border-b  border-red-600">
-      <Link href="/about">About us</Link>
-      <Link href="/products">Products</Link>
-      <Link href="/services">Services</Link>
-      <Image src="/Logo.png" alt="logo" width={300} height={300} />
-      <Link href="/library">Library</Link>
-      <Link href="/career">Career</Link>
-      <Link href="/contact">Contact us</Link>
+    <nav className="trapezoid-wrapper sticky ">
+      <div className="trapezoid relative top-0 z-50 mx-auto flex w-[70rem] items-center justify-center gap-8 bg-[#F9F6EE] uppercase shadow-2xl border-b-2 nav-border-color">
+        <Link href="/about">About us</Link>
+        <Link href="/products">Products</Link>
+        <Link href="/services">Services</Link>
+        <Image src="/Logo.png" alt="logo" width={300} height={300} />
+        <Link href="/library">Library</Link>
+        <Link href="/career">Career</Link>
+        <Link href="/contact">Contact us</Link>
+      </div>
     </nav>
   );
 }
